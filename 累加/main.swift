446ca53c9936afa4ac90a798e0a7e0057abdcee2
20151104705 <lwbJY666>
2017-03-01 10:35:34 +0800

    累加
    1到100

---
+++ 累加/main.swift
@@ -0,0 +1,18 @@
+//
+//  main.swift
+//  累加
+//
+//  Created by 20151104705 on 2017/3/1.
+//  Copyright © 2017年 20151104705. All rights reserved.
+//
+
+import Foundation
+
+var sum = 0
+for i in 0...100
+{
+    sum=sum+i;
+   print(sum)
+}
+
+
